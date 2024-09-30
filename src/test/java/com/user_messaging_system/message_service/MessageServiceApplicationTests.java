a789a7e55b00810579f73c5fd70b1d627d353ca4
burakyapici747 <burakyapici747@gmail.com>
2024-09-30 20:32:20 +0300

    message-service init

---
+++ src/test/java/com/user_messaging_system/message_service/MessageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.user_messaging_system.message_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
