f985df694fa0f5c9eb45a047d3241ec7702e0ec0
burak <burakyapici747@gmail.com>
2024-11-12 19:33:55 +0300

    fix: explicitly import JWTService in AuthenticationService and MessageService

--- src/main/java/com/user_messaging_system/message_service/MessageServiceApplication.java
+++ src/main/java/com/user_messaging_system/message_service/MessageServiceApplication.java
@@ -1,9 +1,12 @@
 package com.user_messaging_system.message_service;
 
+import com.user_messaging_system.core_library.service.JWTService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication(scanBasePackages = {"com.user_messaging_system.core_library"})
+@SpringBootApplication
+@Import(JWTService.class)
 public class MessageServiceApplication {
 
 	public static void main(String[] args) {
