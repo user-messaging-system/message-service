7a6f1cb2994b24ecb2995b3cae4366541a6ea4b0
Burak <burakyapici747@gmail.com>
2024-11-25 17:53:51 +0300

    refactor(api-gateway, user-service): rename endpoints to comply with RESTful API conventions

--- src/main/java/com/user_messaging_system/message_service/api/MessageController.java
+++ src/main/java/com/user_messaging_system/message_service/api/MessageController.java
@@ -10,13 +10,12 @@ import com.user_messaging_system.message_service.service.MessageService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
-
-import static com.user_messaging_system.message_service.common.constant.ApiConstant.*;
+import static com.user_messaging_system.core_library.common.constant.APIConstant.*;
+import static com.user_messaging_system.core_library.common.constant.MessageConstant.*;
 
 @RestController
-@RequestMapping(API_VERSION)
+@RequestMapping(MESSAGE_SERVICE_BASE_URL)
 public class MessageController {
     private final MessageService messageService;
 
@@ -24,20 +23,19 @@ public class MessageController {
         this.messageService = messageService;
     }
 
-    @GetMapping("/conversations/{senderId}/{receiverId}")
+    @GetMapping(PAIR_PATH)
     public ResponseEntity<SuccessResponse<List<MessageGetOutput>>> getMessagesBetweenUsers(
             @RequestHeader("Authorization") String jwtToken,
-            @PathVariable String senderId,
-            @PathVariable String receiverId
+            @RequestParam String senderId,
+            @RequestParam String receiverId
     ){
         SuccessResponse<List<MessageGetOutput>> response = new SuccessResponse.Builder<List<MessageGetOutput>>()
-            .message(MESSAGE_SUCCESSFULLY_RETRIEVED_MESSAGE)
+            .message(MESSAGES_SUCCESSFULLY_RETRIEVED)
             .data(MessageMapper.INSTANCE.messageDtoListToMessageGetOutputList(
                     messageService.getMessagesBetweenUsers(jwtToken, senderId, receiverId))
             )
             .status(HttpStatus.OK.toString())
             .build();
-
         return ResponseEntity.ok(response);
     }
 
@@ -53,7 +51,7 @@ public class MessageController {
             @RequestBody MessageUpdateInput messageUpdateInput
     ){
         SuccessResponse<MessageUpdateOutput> response = new SuccessResponse.Builder<MessageUpdateOutput>()
-            .message(MESSAGE_SUCCESSFULLY_UPDATED_MESSAGE)
+            .message(MESSAGE_SUCCESSFULLY_UPDATED)
             .data(MessageMapper.INSTANCE.messageDtoToMessageUpdateOutput(
                     messageService.updateMessageById(messageId, jwtToken, messageUpdateInput)
             ))
